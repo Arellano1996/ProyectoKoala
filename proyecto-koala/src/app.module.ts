e4948caa9aca55d8e57ba7a555e174e7c07c58da
LAPTOP-1O2LKCCS\Luis <luis.arellano@narancia.com>
2023-12-14 09:31:33 -0600

    Configración de variables de entorno y conexión a base de datos

--- proyecto-koala/src/app.module.ts
+++ proyecto-koala/src/app.module.ts
@@ -2,12 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public')
-    })
+    }),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_Name,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true//Solo en desarollo, sirve para cuando se crea o modifica la base de datos se actualize
+      //En producción se suele hacer una migración
+    }),
   ],
   controllers: [AppController],
   providers: [],
