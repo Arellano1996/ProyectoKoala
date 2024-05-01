6f225965b232cf7212f5207d8d475b50941686db
Arellano1996 <arellano9627@gmail.com>
2024-05-01 15:34:07 -0600

    Se eliminaros Entidades que no son necesarias ya que TypeORM hace las tablas de relacion en automatico

--- proyecto-koala/src/usuarios-canciones-letras/usuarios-canciones-letras.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsuariosCancionesLetrasService } from './usuarios-canciones-letras.service';
-import { UsuariosCancionesLetrasController } from './usuarios-canciones-letras.controller';
-
-@Module({
-  controllers: [UsuariosCancionesLetrasController],
-  providers: [UsuariosCancionesLetrasService],
-})
-export class UsuariosCancionesLetrasModule {}
