d3bb1f1e1ec3aae03260580264fe18d42035d03a
Arellano1996 <arellano9627@gmail.com>
2024-08-30 19:13:16 -0600

    no message

--- LumbreRegiaJr/src/app/app.module.ts
+++ LumbreRegiaJr/src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { environments } from '../environments/environments';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    SharedModule,
-    HttpClientModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-  exports: []
-})
+
+const config: SocketIoConfig = {
+    url: environments.urlServido, options: { }
+}
+
+@NgModule({ 
+    declarations: [
+        AppComponent,
+    ],
+    bootstrap: [AppComponent],
+    exports: [], 
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        SharedModule,
+        SocketIoModule.forRoot( config )
+    ], 
+        providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
